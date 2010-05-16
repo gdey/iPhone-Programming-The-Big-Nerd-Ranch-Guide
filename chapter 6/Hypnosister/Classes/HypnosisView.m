c6c0fa47501fc7f04d5778730cb0c02dbd323826
Gautam Dey <gautam.dey77@gmail.com>
2010-05-15 18:18:20 -0700

    Added Text and shadow.

--- chapter 6/Hypnosister/Classes/HypnosisView.m
+++ chapter 6/Hypnosister/Classes/HypnosisView.m
@@ -39,7 +39,30 @@
     for (float currentRadius = maxRadius; currentRadius > 0; currentRadius -= 20) {
         CGContextAddArc(context, center.x, center.y, currentRadius, 0.0, M_PI * 2.0, YES);
         CGContextStrokePath(context);
+        [[UIColor colorWithRed:sin(rand()) green:sin(rand()) blue:sin(rand()) alpha:1.0] setStroke];
+            //        [[UIColor colorWithWhite:sin(currentRadius) alpha:1.0] setStroke];
     }
+    
+        // Add some text.
+    NSString *text = @"You are getting sleepy";
+    UIFont *font = [UIFont boldSystemFontOfSize:28];
+    
+    CGRect textRect;
+    textRect.size = [text sizeWithFont:font];
+    textRect.origin.x = center.x - ( textRect.size.width  / 2.0 );
+    textRect.origin.y = center.y - ( textRect.size.height / 2.0 );
+    
+        // Set the fill color
+    [[UIColor blackColor] setFill];
+    
+        // Set the shadow
+    CGSize offset = CGSizeMake(4, 3);
+    CGColorRef color = [[UIColor darkGrayColor] CGColor];
+    CGContextSetShadowWithColor(context, offset, 2.0, color);
+    
+        // Draw the string
+    [text drawInRect:textRect withFont:font];
+    
 }
 
 
