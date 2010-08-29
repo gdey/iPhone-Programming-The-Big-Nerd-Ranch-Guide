ab6feb72aaf2a463b9365a8841bff46ef74f4e6b
Gautam Dey <gautam.dey77@gmail.com>
2010-08-29 11:50:35 -0700

    Initial Commit

---
+++ Whereami/Classes/WhereamiAppDelegate.h
@@ -0,0 +1,20 @@
+//
+//  WhereamiAppDelegate.h
+//  Whereami
+//
+//  Created by Gautam Dey on 8/28/10.
+//  Copyright __MyCompanyName__ 2010. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import <CoreLocation/CoreLocation.h>
+
+@interface WhereamiAppDelegate : NSObject <UIApplicationDelegate, CLLocationManagerDelegate> {
+    UIWindow *window;
+    CLLocationManager *locationManager;
+}
+
+@property (nonatomic, retain) IBOutlet UIWindow *window;
+
+@end
+
