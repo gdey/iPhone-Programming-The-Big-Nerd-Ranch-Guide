97c14271ef19ffc7fd5877b21eed79eec4e9acfb
Gautam Dey <gautam.dey77@gmail.com>
2010-08-31 21:48:21 -0700

    Added Scrollview

--- Hypnosister/Classes/HypnosisterAppDelegate.m
+++ Hypnosister/Classes/HypnosisterAppDelegate.m
@@ -22,12 +22,32 @@
     // Override point for customization after application launch.
 	
     CGRect wholeWindow = [window bounds];
-    view = [[HypnosisView alloc] initWithFrame:wholeWindow];
+    
+    UIScrollView *scrollView = [[UIScrollView alloc] initWithFrame:wholeWindow];
+    [window addSubview:scrollView];
+    
+        // Make your view twice as large as the window
+    CGRect reallyBigRect;
+    reallyBigRect.origin = CGPointZero;
+    reallyBigRect.size.width = wholeWindow.size.width * 2.0;
+    reallyBigRect.size.height = wholeWindow.size.height * 2.0;
+    [scrollView setContentSize:reallyBigRect.size];
+    
+        // Center it in the scroll view
+    CGPoint offset;
+    offset.x = wholeWindow.size.width * 0.5;
+    offset.y = wholeWindow.size.height * 0.5;
+    [scrollView setContentOffset:offset];
+    
+    
+        // Create the view
+    view = [[HypnosisView alloc] initWithFrame:reallyBigRect];
     [view setBackgroundColor:[UIColor clearColor]];
-    [window addSubview:view];
+    [scrollView addSubview:view];
+    
     
     [window makeKeyAndVisible];
-	
+	[scrollView release];
 	return YES;
 }
 
