66068e45fd7818187593b80dfdc606e9f0edc77d
Gautam Dey <gautam.dey77@gmail.com>
2010-09-04 00:06:35 -0700

    Initial and add the initial notifications code.

---
+++ HeavyRotation/Classes/HeavyRotationAppDelegate.h
@@ -0,0 +1,18 @@
+//
+//  HeavyRotationAppDelegate.h
+//  HeavyRotation
+//
+//  Created by Gautam Dey on 9/3/10.
+//  Copyright __MyCompanyName__ 2010. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface HeavyRotationAppDelegate : NSObject <UIApplicationDelegate> {
+    UIWindow *window;
+}
+
+@property (nonatomic, retain) IBOutlet UIWindow *window;
+
+@end
+
